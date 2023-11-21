e5ed2223a341815e0ddc825e32797090d05a7d79
Emilia <rybicka.emilka@gmail.com>
2023-11-21 08:29:30 +0100

    Moduł 3: Swagger

--- src/main/java/pl/codemiry/ksb2/Ksb2Application.java
+++ src/main/java/pl/codemiry/ksb2/Ksb2Application.java
@@ -2,12 +2,28 @@ package pl.codemiry.ksb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 
 @SpringBootApplication
+@EnableWebMvc
 public class Ksb2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Ksb2Application.class, args);
     }
 
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
